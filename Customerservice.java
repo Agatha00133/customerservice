c3be888e2533cceee6d06a669c7bb16f7fa5be36
Agatha00133 <agathansofor1@gmail.com>
2025-12-06 21:35:26 +0100

    Add files via upload

---
+++ Customerservice.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+
+ public class CustomerService{
+       static viod main (string[]args);
+       Scanner inputCollector = new Scanner(system.in);
+       string menu = """
+       Noka phone customerservice menu{
+     
+     1.Airtime $ date
+     2.Account information 
+     3.network $ connectivity 
+     4.Phone settings
+     5.messaging
+     6.services Call
+     7.customer support
+     8.phone maintenace
+     9.exit
+     """,
+    system.out.print(menu);
+
+    int generalMenuChoice =
+    inputCollector.nextInt();
+    switch( generalMenuChoice ){
+     """;
+    System.out.print(menu)
+    
+   case 1->
+   {system.out.println("Airtime $ date menu");
+   string Airtime & date Menu = """
+   1.1- check airtime
+   1.2- data balance 
+   1.3- buy airtime
+   """;
+   system.out.println("Airtime $ date menu")
+  int Airtime $ date MenuChoice = 
+    inputCollector.nextInt();
+    switch("Airtime $ date menu"){
+
+     case 1->{system.out.print("wait a minute");  
+    string Airtime$datecheckairtimemenu = """
+   enter 1 to stop
+   """;
+  system.out.println(Airtime$date checkairtimemenuChoice );
+  int Airtime$date checkairtimemenuChoice 
+  inputCollector.nextInt();
+ switch( Airtime$date checkairtimemenuChoice ){
+case 1->system.out.print(let gooo); 
+}
+  }  
+  
+  case 2->{system.out.printf("Account information menu");
+  string"Account information menu="""
+
+  2.1- view my number
+  2.2- account status
+  2.3-update profile
+  """;
+  system.out.println(Account information menu);
+  int Account information menuChoice =
+  inputCollector.nextInt();
+  switch(Account information menuChoice){
+
+  case 1-> system.out.print("wake up oooo");
+
+  case 2-> system.out.print("i seee uooo000");
+  default-> system.out.println("oga,you known go school");
+}
+
+]   
+
+
+ case 3-> system.out.printf("network&connectivitymenu");
+ string network&connectivitymenu ="""
+ 
+  1.settings
+  2.check nework issue
+  """;
+  system.out.println("phone settings menu");
+  int phone settings menuchoice =
+  inputcollector.nextInt();
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
